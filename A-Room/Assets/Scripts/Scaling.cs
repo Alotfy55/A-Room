1ea9c88a6d184aa6f623ecdc9e22ba198bffa9fd
Ahmedyasser11 <53158750+Ahmedyasser11@users.noreply.github.com>
2022-04-11 18:51:09 +0200

    Scale update

---
+++ A-Room/Assets/Scripts/Scaling.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaling : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private float GetScale(int width, int height, Vector2 scalerReferenceResolution, float scalerMatchWidthOrHeight)
+    {
+        return Mathf.Pow(width / scalerReferenceResolution.x, 1f - scalerMatchWidthOrHeight) *
+               Mathf.Pow(height / scalerReferenceResolution.y, scalerMatchWidthOrHeight);
+    }
+}
