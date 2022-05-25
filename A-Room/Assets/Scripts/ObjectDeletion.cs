caa74e7f183671d4d2b46a8de1606c833efcfeb0
omaaressaam <omaar.essaam215@gmail.com>
2022-05-25 21:10:23 +0200

    trying long press

--- A-Room/Assets/Scripts/ObjectDeletion.cs
+++ A-Room/Assets/Scripts/ObjectDeletion.cs
@@ -35,28 +35,58 @@ public class ObjectDeletion : MonoBehaviour
         arCamera = GameObject.Find("AR Camera").GetComponent<Camera>();  // get the ar camera 
     }
 
-    // Update is called once per frame
+
+    private float timePressed = 0.0f;
+    private float timeLastPress = 0.0f;
+    public float timeDelayThreshold = 1.0f;
     void Update()
     {
-        if (Input.touchCount == 2)   // touch occured
-        {
-            if (arRaycastManager.Raycast(Input.GetTouch(0).position, hits)) // whether touch hits a detected plane plane
-            {
-                if (Input.GetTouch(0).phase == TouchPhase.Stationary) // If a hold occured
-                {
-                    RaycastHit hit;
-                    Ray ray = arCamera.ScreenPointToRay(Input.GetTouch(0).position);
+        
 
-                    if (Physics.Raycast(ray, out hit)) // cast a ray from ray to screen 
+    void Update()
+    {
+        checkForLongPress(timeDelayThreshold);
+    }
+
+    void checkForLongPress(float tim)
+    {
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        { // If the user puts her finger on screen...
+            timePressed = Time.time - timeLastPress;
+        }
+
+        if (Input.GetTouch(0).phase == TouchPhase.Ended)
+        { // If the user raises her finger from screen
+            timeLastPress = Time.time;
+            if (timePressed > tim)
+            { // Is the time pressed greater than our time delay threshold?
+                Updatee();
+            }
+        }
+    }
+
+}
+    // Update is called once per frame
+    void Updatee()
+    {
+    
+        if (arRaycastManager.Raycast(Input.GetTouch(0).position, hits)) // whether touch hits a detected plane plane
+            {
+            if (Input.GetTouch(0).phase == TouchPhase.Stationary) // If a hold occured
+            {
+                RaycastHit hit;
+                Ray ray = arCamera.ScreenPointToRay(Input.GetTouch(0).position);
+
+                if (Physics.Raycast(ray, out hit)) // cast a ray from ray to screen 
+                {
+                    if (hit.collider.gameObject.tag.Equals("FurnitureModels")) // if the ray collides with an object whose tag "FurnitureModels"
                     {
-                        if (hit.collider.gameObject.tag.Equals("FurnitureModels")) // if the ray collides with an object whose tag "FurnitureModels"
-                        {
-                            parent = hit.collider.gameObject.transform;
-                            SwitchShowHide();
-                        }
+                        parent = hit.collider.gameObject.transform;
+                        SwitchShowHide();
                     }
                 }
             }
         }
+        
     }
 }
